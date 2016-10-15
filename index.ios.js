939584c8884b9fe02597f88ac4b2a6dbb6ea97be
Andrew Hird <andrew.james.hird@gmail.com>
2016-10-15 15:32:45 +1300

    state

--- index.ios.js
+++ index.ios.js
@@ -1,36 +1,36 @@
- import React, { Component } from 'react'
- import { AppRegistry, Image, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { AppRegistry, Text, View } from 'react-native'
 
- class Bananas extends Component {
-   render() {
-     let pic = {
-       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-     }
-     return (
-       <Image source={pic} style={{width: 193, height: 110}}/>
-     )
-   }
- }
+class Blink extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {showText: true};
 
- class Greeting extends Component {
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText })
+    }, 1000)
+  }
+
   render() {
+    let display = this.state.showText ? this.props.text : ' '
     return (
-      <Text>Hello {this.props.name}!</Text>
-    );
+      <Text>{display}</Text>
+    )
   }
 }
 
- class Props extends Component {
-   render() {
-     return (
-       <View style={{alignItems: 'center', marginTop: 20}}>
-       <Bananas />
-         <Greeting name='Bob' />
-         <Greeting name='Jane' />
-         <Greeting name='Greg' />
-       </View>
-     );
-   }
- }
+class BlinkApp extends Component {
+  render() {
+    return (
+      <View style={{marginTop: 20}}>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    )
+  }
+}
 
- AppRegistry.registerComponent('reactNativeDemo', () => Props)
+AppRegistry.registerComponent('reactNativeDemo', () => BlinkApp)
